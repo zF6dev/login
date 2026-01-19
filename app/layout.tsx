f2a2c3bda17898a10c2fb114bc4960f44e7f1595
zF6dev <dev@9a.si>
2026-01-19 21:46:39 +0900

    metadataの変更=Webページのタイトルや説明の変更を行なった。

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "席予約システム v0.1",
+  description: "Next.jsとSupabaseを用いたログインシステム。",
 };
 
 const geistSans = Geist({
