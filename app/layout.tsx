ffb142c4b47619c29de69a05c50ce26a02a5cd70
zF6dev <dev@9a.si>
2026-01-20 00:12:57 +0900

    ダークモード・ライトモードの切り替えボタンを復活させた。 それに伴ってapp/layout.tsxにヘッダーを作成し、ソースコードの表示もできるようにした。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import Link from "next/link";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -26,6 +28,21 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <header className="border-b border-slate-200">
+            <div className="mx-auto max-w-5xl h-14 px-6 flex items-center justify-between">
+              <Link href="/">Home</Link>
+              <Link
+                href="https://github.com/zF6dev/login"
+                target="_blank"
+                className="absolute right-10 text-slate-1000 hover:underline underline-offset-4"
+              >
+                GitHub →
+              </Link>
+              <div className="ml-auto flex items-center gap-4">
+                <ThemeSwitcher />
+              </div>
+            </div>
+          </header>
           {children}
         </ThemeProvider>
       </body>
