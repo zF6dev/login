9bf96620045fc3cbb22ce3315ddad9b44d1f549c
zF6dev <dev@9a.si>
2026-01-20 17:16:33 +0900

    not-found.tsxを作成し、ランダムで英語版と日本語版を表示するようにした。 隠しコマンドとかも追加したいかもしれない。

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Zen_Maru_Gothic } from "next/font/google"
+import { useState, useEffect, JSX } from "react"
+
+const zen_maru_gothic = Zen_Maru_Gothic({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export default function NotFound() {
+  const [selectedMain, setSelectedMain] = useState<JSX.Element | null>(null);
+
+  useEffect(() => {
+    const mains = [
+      <main key="english" className={zen_maru_gothic.className}>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <h1 className="text-3xl">404</h1>
+          <br></br>
+          <h2 className="text-xl">Page not found looooooooool</h2>
+        </div>
+      </main>,
+      <main key="japanese" className={zen_maru_gothic.className}>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <h1 className="text-3xl">あらまぁ、サーバーが頭を掻いてるよ</h1>
+        </div>
+      </main>
+    ];
+    const randomIndex = Math.floor(Math.random() * mains.length);
+    setSelectedMain(mains[randomIndex]);
+  }, []);
+
+  return selectedMain;
+}
