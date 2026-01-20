8be5d01f74e946dc9b7945250035a6f4bb8d137f
zF6dev <dev@9a.si>
2026-01-20 23:29:16 +0900

    やっぱりnot-found.tsxはクライアント側で実行する。 デプロイ後にうまく動作しなかったから。

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,41 +1,34 @@
+"use client";
+
 import { Zen_Maru_Gothic } from "next/font/google"
-import { headers } from "next/headers"
-import { JSX } from "react"
+import { useState, useEffect, JSX } from "react"
 
 const zen_maru_gothic = Zen_Maru_Gothic({
   weight: '400',
   subsets: ['latin'],
 })
 
-export default async function NotFound() {
-  const h = await headers();
-  const userAgent = h.get('user-agent');
+export default function NotFound() {
+  const [selectedMain, setSelectedMain] = useState<JSX.Element | null>(null);
 
-  const strings: {
-    key: string,
-    contents: JSX.Element
-  }[] = [
-      {
-        key: "english",
-        contents: <>
-          <h1 className="text-3xl">404</h1>
-          <br />
-          <h2 className="text-xl">Page not found looooooooool</h2>
-        </>
-      },
-      {
-        key: "japanese",
-        contents: <h1 className="text-3xl">あらまぁ、サーバーが頭を掻いてるよ</h1>
-      }
+  useEffect(() => {
+    const mains = [
+      <main key="english" className={zen_maru_gothic.className}>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <h1 className="text-5xl">404</h1>
+          <br></br>
+          <h2 className="text-2xl">Page not found looooooooool</h2>
+        </div>
+      </main>,
+      <main key="japanese" className={zen_maru_gothic.className}>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <h1 className="text-5xl">あらまぁ、サーバーが頭を掻いてるよ</h1>
+        </div>
+      </main>
     ];
+    const randomIndex = Math.floor(Math.random() * mains.length);
+    setSelectedMain(mains[randomIndex]);
+  }, []);
 
-  const randomIndex = Math.floor(Math.random() * strings.length);
-
-  return (
-    <main key={strings[randomIndex].key} className={zen_maru_gothic.className}>
-      <div className="flex flex-col items-center justify-center min-h-screen absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        {strings[randomIndex].contents}
-      </div>
-    </main>
-  );
+  return selectedMain;
 }
